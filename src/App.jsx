425aed590f81a8ccac3df6a12d89986ca7768139
unknown <hosein>
2023-12-21 10:54:52 +0330

    install ract-query and react-que-devtools  package and setup in  project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./router";
 import GlobalStyles from "./styles/GlobalStyles";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 };
 
