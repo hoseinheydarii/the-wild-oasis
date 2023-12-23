3ee04d789941a9f48ee6ecbcb532b22f8a3296d2
unknown <hosein>
2023-12-23 23:12:17 +0330

    sort and filter server side

--- src/features/bookings/hooks/useBookings.js
+++ src/features/bookings/hooks/useBookings.js
@@ -1,14 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export const useBookings = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  //FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  const sortByRow = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRow.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, bookings, error };
 };
