16ee5a0988d67832e336f133355c4e2c0f90abbc
unknown <hosein>
2023-12-25 23:43:52 +0330

    -A

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./hooks/useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./hooks/useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/hooks/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +11,32 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+const DashboardLayout = () => {
+  const { isLoading: isLoading3, cabins } = useCabins();
+  const { isLoading, bookings } = useRecentBookings();
+  const {
+    isLoading: isLoading2,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+
+  if (isLoading || isLoading2 || isLoading3) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>today</div>
+      <div>chart</div>
+      <div>chart2</div>
+    </StyledDashboardLayout>
+  );
+};
+
+export default DashboardLayout;
