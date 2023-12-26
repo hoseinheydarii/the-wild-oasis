c8ccb8132e93a0d0f6f454d279dd7d00a2d6d94d
unknown <hosein>
2023-12-26 22:16:02 +0330

    install recharts and use them

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./hooks/useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/hooks/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,6 +34,7 @@ const DashboardLayout = () => {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
